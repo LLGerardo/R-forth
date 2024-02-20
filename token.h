61eda3324acd51ce59591860af14f07171bf05fd
Laura Lupita Gerardo <104596023+LLGerardo@users.noreply.github.com>
2024-02-20 16:07:13 +0000

    adjust token.h

---
+++ token.h
@@ -0,0 +1,18 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+// Enum for token types,..,m,
+typedef enum {
+    NUMBER,
+    OPERATOR,
+    SYMBOL,
+    WORD
+} token_type_t;
+
+//create abstract datatype
+typedef struct token_t{
+    token_type_t type;
+    char *text;
+} token_t;
+
+#endif
